45271c7ed3a25167099cf774c95b83715e2cc3fd
krishnaanshgoel <151033376+krishnaanshgoel@users.noreply.github.com>
2024-06-27 02:06:39 +0530

    committed

--- currcon/backend/index.js
+++ currcon/backend/index.js
@@ -3,12 +3,7 @@ const cors=require("cors");
 const Rootrouter=require("./routes/main");
 const app=express();
 app.use(cors());
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://convert-ease-nine.vercel.app');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-})
+
 app.use(express.json());
 
 app.use("/",Rootrouter)
